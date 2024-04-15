75de33124f4c1c11c25aa27b0739deee7a0f69d8
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-04-15 09:51:47 -0400

    Added build tree method in tree class

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 class Node {
     constructor(data) {
         this.data = data;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -13,9 +13,28 @@ class Tree {
     }
 
     buildTree(arr) {
-        let sortArray = arr.sort((a, b) => (a - b));
-        console.log(sortArray);
-        return sortArray
+        // Sort array and remove duplicate values
+        arr = Array.from(new Set(arr.sort((a, b) => (a - b))));
+
+        function buildBST(start, end) {
+            if (start > end) {
+                return null;
+            }
+
+            // Find middle
+            let mid = Math.floor((start + end) / 2)
+
+            // Create tree node with mid as root
+            const node = new Node(arr[mid])
+
+            node.left = buildBST(start, mid - 1)
+            node.right = buildBST(mid + 1, end)
+
+            return node
+        }
+
+        // Call the recursive function to build the tree
+        return buildBST(0, arr.length - 1);
     }
 }
 
@@ -32,5 +51,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
-let tree = new Tree([30, 70, 10, 60, 90]);
-console.log(tree)
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array)
+console.log(prettyPrint(tree.root))
